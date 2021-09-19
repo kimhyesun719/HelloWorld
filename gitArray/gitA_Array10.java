324032db2758d6477ce1f4fb9f7391c5ac8914e9
kimhyesun719 <89463167+kimhyesun719@users.noreply.github.com>
2021-09-19 21:16:49 +0900

    Add files via upload

---
+++ gitArray/gitA_Array10.java
@@ -0,0 +1,21 @@
+package com.kh.gitArray;
+
+public class gitA_Array10 {
+
+	public static void main(String[] args) {
+		
+		// 각 인덱스에 과일 다섯개를 담고 그중 귤 출력 하기
+		
+		String[] arr = new String[5];
+		
+		arr[0] = "사과";
+		arr[1] = "귤";
+		arr[2] = "포도";
+		arr[3] = "복숭아";
+		arr[4] = "참외";
+		
+		System.out.println(arr[1]);
+
+	}
+
+}
